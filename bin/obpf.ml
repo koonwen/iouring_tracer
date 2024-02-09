533826442a74847f19d9224807f8d653c2cc4251
Lee Koon Wen <koonwen@gmail.com>
2024-02-09 21:21:11 +0800

    Cmdliner work

--- bin/obpf.ml
+++ bin/obpf.ml
@@ -5,6 +5,10 @@ let obpf bpftrace_prog trace_file bin =
 
 open Cmdliner
 
+let gen_cmd =
+  let info = Cmd.info "gen" in
+  Cmd.v info Term.(const ())
+
 let log_file =
   let doc = "write trace log to FILE" in
   Arg.(value & opt file "trace.txt" & info [ "l" ] ~docv:"FILE" ~doc)
@@ -14,13 +18,17 @@ let bpftrace_prog =
   Arg.(value & opt file "tracepoints.bt" & info [ "p" ] ~docv:"FILE" ~doc)
 
 let program =
-  let doc = "program to execute" in
+  let doc = "program to trace" in
   Arg.(required & pos 0 (some string) None & info [] ~docv:"PROGRAM" ~doc)
 
-let cmd =
+let trace_cmd =
   let doc = "Program tracer tool using bpftrace" in
   let man = [ `S Manpage.s_description ] in
-  let info = Cmd.info "obpf" ~version:"%%VERSION%%" ~doc ~man in
+  let info = Cmd.info "trace" ~version:"%%VERSION%%" ~doc ~man in
   Cmd.v info Term.(const obpf $ bpftrace_prog $ log_file $ program)
 
-let () = exit (Cmd.eval cmd)
+let trace =
+  let doc = Cmd.info "obpf" in
+  Cmd.group doc [ gen_cmd; trace_cmd ]
+
+let () = exit (Cmd.eval trace)
