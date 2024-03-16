846464995ea97a0306b6ec0f4acea3635136f294
Lee Koon Wen <koonwen@gmail.com>
2024-03-16 16:42:16 +0800

    Cleaning up and start work on user accessible custom events

--- lib/driver.ml
+++ lib/driver.ml
@@ -1,22 +1,26 @@
 open Unix
 
-type 'a program = Binary of string | Function of (unit -> 'a)
+type program = Function | Binary of string
 
-let runner ~(input : Bpftrace.arg) ?(output = "io_uring.events") ?ev_handler_op
-    (f : 'a program) =
+let runner ~(input : Bpftrace.arg) output (f : program) =
   if not Sys.unix then failwith "this system is not supported";
   if getuid () <> 0 then failwith "bpftrace needs to be run as root";
-  openfile output [ O_CLOEXEC; O_CREAT; O_TRUNC ] 0o600 |> close;
   match fork () with
   | 0 -> (
-      match f with Binary bin -> system bin |> ignore | Function f -> f ())
-  | bpftrace_pid -> (
+      (* Wait for bpftrace to initialize *)
+      sleepf 2.0;
+      match f with Function -> () | Binary bin -> ignore (system bin))
+  | user_prog_pid -> (
+      let output = Format.sprintf "%d_%s" user_prog_pid output in
+      openfile output [ O_CLOEXEC; O_CREAT; O_TRUNC ] 0o600 |> close;
       match fork () with
-      | 0 -> Runtime_events_hook.trace_poll ?ev_handler_op output
+      | 0 -> Runtime_events_hook.trace_poll output
       | trace_poll_pid ->
           at_exit (fun () ->
               kill trace_poll_pid Sys.sigint;
-              kill bpftrace_pid Sys.sigint);
+              kill user_prog_pid Sys.sigint);
           Bpftrace.(
-            make ~output ~proc_management:(PID bpftrace_pid) input
+            make ~output ~proc_management:(PID user_prog_pid) input
             |> exec |> ignore))
+
+let start () = runner ~input:Bpftrace.Default "io_uring.events" Function
--- lib/runtime_events_hook.ml
+++ lib/runtime_events_hook.ml
@@ -1,4 +1,4 @@
-include Runtime_events
+open Runtime_events
 
 type User.tag += Io_uring
 
@@ -14,25 +14,16 @@ let ty =
 
 let bpftrace_ev = User.register "io-uring" Io_uring ty
 
-let get_events ic =
+let rec get_events ic =
   match In_channel.input_line ic with
   | None -> ()
-  | Some ev -> User.write bpftrace_ev ev
+  | Some ev ->
+    User.write bpftrace_ev ev;
+    get_events ic
 
-let bpftrace_ev_handler _i _ts ty s =
-  match User.tag ty with Io_uring -> Printf.printf "%s\n%!" s | _ -> ()
-
-let trace_poll ?ev_handler_op filename =
-  let ev_handler = Option.value ~default:bpftrace_ev_handler ev_handler_op in
+let trace_poll filename =
   In_channel.with_open_text filename (fun ic ->
-      start ();
-      let cursor = create_cursor None in
-      let cb = Callbacks.create () |> Callbacks.add_user_event ty ev_handler in
       while true do
         get_events ic;
-        ignore (read_poll cursor cb None);
         Unix.sleepf 0.1
-      done;
-      (* In case something was missed *)
-      ignore (read_poll cursor cb None);
-      free_cursor cursor)
+      done)
